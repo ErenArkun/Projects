c253f8bd3711ac283a085dd2b2ca39eafdf66b8a
ErenArkun <70512163+ErenArkun@users.noreply.github.com>
2023-11-27 04:30:10 +0300

    git push REMOTE-NAME BRANCH-NAME

---
+++ 3-Automatic Trash Box/TrashBox.ino
@@ -0,0 +1,34 @@
+#include <Servo.h>
+
+const int trigPin = 2;
+const int echoPin = 3;
+const int servoPin = 9;
+
+Servo myServo;
+
+void setup(){
+  Serial.begin(9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  myServo.attach(servoPin);
+}
+
+void loop(){
+  long duration, distance;
+
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoEPin, HIGH);
+
+  distance = (duration / 2) * 0.0344;
+
+  if (distance < 10){
+    myServo.write(90);
+    delay(500);
+    myServo.write(0);
+  }
+  myServo.write(1000);
+}
