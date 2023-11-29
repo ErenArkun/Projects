f7e21f97b1e6dfe999d8623f0f549e53555d926f
ErenArkun <70512163+ErenArkun@users.noreply.github.com>
2023-11-29 03:23:47 +0300

    commit & push

--- 7-Line Tracking Robot/robot.ino
+++ 7-Line Tracking Robot/robot.ino
@@ -2,58 +2,62 @@
 #define SensorOrta 11
 #define SensorSag 12
 #define MotorR1 6
-#define MotorR2 7 // Sensör ve Motorların Arduino'ya bağladığımız pinlerini tanımlıyoruz.
+#define MotorR2 7
 #define MotorRE 9
 #define MotorL1 5
 #define MotorL2 4
 #define MotorLE 3
+
 void setup() {
   pinMode(SensorSol, INPUT);
   pinMode(SensorOrta, INPUT);
   pinMode(SensorSag, INPUT);
-  pinMode(MotorR1, OUTPUT); // Sensör ve Motorların Giriş-Çıkış bilgilerini belirtiyoruz.
+  pinMode(MotorR1, OUTPUT);
   pinMode(MotorR2, OUTPUT);
   pinMode(MotorL1, OUTPUT);
   pinMode(MotorL2, OUTPUT);
 }
 
 void loop() {
-  if (digitalRead(SensorSol) == 0 && digitalRead(SensorOrta) == 1 && digitalRead(SensorSag) == 0) { // Orta sensör çizgiyi gördüğünde robot ileri gitsin.
+  if (digitalRead(SensorSol) == 0 && digitalRead(SensorOrta) == 1 && digitalRead(SensorSag) == 0) {
     ileri();
   }
-  if (digitalRead(SensorSol) == 0 && digitalRead(SensorOrta) == 0 && digitalRead(SensorSag) == 1) { // Sağ sensör çizgiyi gördüğünde robot sağa dönsün.
+  if (digitalRead(SensorSol) == 0 && digitalRead(SensorOrta) == 0 && digitalRead(SensorSag) == 1) {
     sag();
   }
-  if (digitalRead(SensorSol) == 1 && digitalRead(SensorOrta) == 0 && digitalRead(SensorSag) == 0) { // Sol sensör çizgiyi gördüğünde robot sola dönsün.
+  if (digitalRead(SensorSol) == 1 && digitalRead(SensorOrta) == 0 && digitalRead(SensorSag) == 0) {
     sol();
   }
 }
 
-void ileri() { // Robotun ileri yönde hareketi için fonksiyon tanımlıyoruz.
-  digitalWrite(MotorR1, HIGH); // Sağ motorun ileri hareketi aktif
-  digitalWrite(MotorR2, LOW); // Sağ motorun geri hareketi pasif
-  analogWrite(MotorRE, 150); // Sağ motorun hızı 150
-  digitalWrite(MotorL1, HIGH); // Sol motorun ileri hareketi aktif
-  digitalWrite(MotorL2, LOW); // Sol motorun geri hareketi pasif
-  analogWrite(MotorLE, 150); // Sol motorun hızı 150
+void ileri() {
+  digitalWrite(MotorR1, HIGH);
+  digitalWrite(MotorR2, LOW);
+  analogWrite(MotorRE, 150);
+  digitalWrite(MotorL1, HIGH);
+  digitalWrite(MotorL2, LOW);
+  analogWrite(MotorLE, 150);
 }
-void sag() { // Robotun sağa dönme hareketi için fonksiyon tanımlıyoruz.
-  digitalWrite(MotorR1, HIGH); // Sağ motorun ileri hareketi aktif
-  digitalWrite(MotorR2, LOW); // Sağ motorun geri hareketi pasif
-  analogWrite(MotorRE, 0); // Sağ motorun hızı 0 (Motor duruyor)
-  digitalWrite(MotorL1, HIGH); // Sol motorun ileri hareketi aktif
-  digitalWrite(MotorL2, LOW); // Sol motorun geri hareketi pasif
-  analogWrite(MotorLE, 150); // Sol motorun hızı 150
+
+void sag() {
+  digitalWrite(MotorR1, HIGH);
+  digitalWrite(MotorR2, LOW);
+  analogWrite(MotorRE, 0);
+  digitalWrite(MotorL1, HIGH);
+  digitalWrite(MotorL2, LOW);
+  analogWrite(MotorLE, 150);
 }
-void sol() { // Robotun sola dönme hareketi için fonksiyon tanımlıyoruz.
-  digitalWrite(MotorR1, HIGH); // Sağ motorun ileri hareketi aktif
-  digitalWrite(MotorR2, LOW); // Sağ motorun geri hareketi pasif
-  analogWrite(MotorRE, 150); // Sağ motorun hızı 150
-  digitalWrite(MotorL1, HIGH); // Sol motorun ileri hareketi aktif
-  digitalWrite(MotorL2, LOW); // Sol motorun geri hareketi pasif
-  analogWrite(MotorLE, 0); // Sol motorun hızı 0 (Motor duruyor)
+
+void sol() {
+  digitalWrite(MotorR1, HIGH);
+  digitalWrite(MotorR2, LOW);
+  analogWrite(MotorRE, 150);
+  digitalWrite(MotorL1, HIGH);
+  digitalWrite(MotorL2, LOW);
+  analogWrite(MotorLE, 0);
 }
-void dur() { // Robotun durma hareketi için fonksiyon tanımlıyoruz.
+
+void dur() {
   digitalWrite(MotorR1, HIGH);
   digitalWrite(MotorR2, LOW);
   digitalWrite(MotorRE, LOW);
--- 7-Line Tracking Robot/sensor test code.ino
+++ 7-Line Tracking Robot/sensor test code.ino
@@ -1,14 +1,16 @@
 #define SensorSol 10
-#define SensorOrta 11   // Sensör pinlerini tanımlıyoruz.
+#define SensorOrta 11
 #define SensorSag 12
+
 void setup() {
   pinMode(SensorSol, INPUT);
-  pinMode(SensorOrta, INPUT); //Sensörlerimizi giriş olarak tanımlıyoruz.
+  pinMode(SensorOrta, INPUT);
   pinMode(SensorSag, INPUT);
-  Serial.begin(9600);  //Seri haberleşmeyi başlatıyoruz.
+  Serial.begin(9600);
   Serial.println("Cizgi Sensoru Testi");
   delay(2000);
 }
+
 void loop() {
   Serial.print(digitalRead(SensorSol));
   Serial.print(",");
